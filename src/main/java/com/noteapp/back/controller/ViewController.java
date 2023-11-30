2dd403e5fd7e27e66f69f128ab5b09e52cde07f6
hjinshin <gudwls818@gmail.com>
2023-11-30 16:42:50 +0900

    View Controller

--- src/main/java/com/noteapp/back/controller/ViewController.java
+++ src/main/java/com/noteapp/back/controller/ViewController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class webSocketController {
+public class ViewController {
     @Value("${server_url}")
     private String server_url;
 
