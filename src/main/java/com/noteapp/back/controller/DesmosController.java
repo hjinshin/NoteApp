744cfde9113bdef94fab1484ffc7dc4817c28e8e
hjinshin <gudwls818@gmail.com>
2023-11-30 16:04:58 +0900

    web socket

--- src/main/java/com/noteapp/back/controller/DesmosController.java
+++ src/main/java/com/noteapp/back/controller/DesmosController.java
@@ -2,7 +2,6 @@ package com.noteapp.back.controller;
 
 import com.noteapp.back.service.DesmosService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
---
+++ src/main/java/com/noteapp/back/controller/selfController.java
@@ -0,0 +1,18 @@
+package com.noteapp.back.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class selfController {
+    @Value("${server_url}")
+    private String server_url;
+
+    @RequestMapping(value = "/desmos")
+    public String desmosPage(Model model) {
+        model.addAttribute("server_url", server_url);
+        return "desmos";
+    }
+}
