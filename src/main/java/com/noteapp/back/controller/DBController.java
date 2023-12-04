738dd5ff14f7a3943396a780a00a057a9c8fef89
hjinshin <gudwls818@gmail.com>
2023-12-05 05:02:34 +0900

    Google 로그인 API 주석 처리

--- src/main/java/com/noteapp/back/controller/DBController.java
+++ src/main/java/com/noteapp/back/controller/DBController.java
@@ -7,14 +7,9 @@ import com.noteapp.back.dto.UpdateDto;
 import com.noteapp.back.service.GoogleOAuthService;
 import com.noteapp.back.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 
@@ -24,39 +19,6 @@ public class DBController {
     private UserService userService;
     @Autowired
     private GoogleOAuthService googleOAuthService;
-
-
-    @Value("${google_clientId}")
-    private String google_clientId;
-    @Value("${google_redirectUrl}")
-    private String google_redirectUrl;
-    @Value("${google_secret}")
-    private String google_secret;
-    @GetMapping("/redirect")
-    public String handleGoogleCallback(@RequestParam("code") String code) {
-        // Exchange authorization code for access token
-        String tokenEndpoint = "https://accounts.google.com/o/oauth2/token";
-        String tokenRequest = "code=" + code
-                + "&client_id=" + google_clientId
-                + "&client_secret=" + google_secret
-                + "&redirect_uri=" + google_redirectUrl
-                + "&grant_type=authorization_code";
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Type", "application/x-www-form-urlencoded");
-
-        HttpEntity<String> request = new HttpEntity<>(tokenRequest, headers);
-
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.exchange(
-                tokenEndpoint,
-                HttpMethod.POST,
-                request,
-                String.class
-        );
-
-        return response.getBody();
-    }
 
     @GetMapping("/api/test")
     public String testGoogleAccessToken(@RequestParam String access_token) {
