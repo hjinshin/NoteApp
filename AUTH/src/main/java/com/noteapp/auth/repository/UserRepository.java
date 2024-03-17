80aaeff1d99fb80b03a0710cb12decf32bab1de1
hjinshin <gudwls818@gmail.com>
2024-03-17 11:25:27 +0900

    OCR server

--- AUTH/src/main/java/com/noteapp/auth/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.noteapp.auth.repository;
-import com.noteapp.auth.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// DAO
-public interface UserRepository extends JpaRepository<User, String> {
-}
